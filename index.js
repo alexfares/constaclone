ecec4aba9c7f50c3787511c03708e22aea9b8f11
Alex Fares <alexanderfares@gmail.com>
2019-11-26 16:25:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-var TelegramBot = require( 'node-telegram-bot-api' );
+const TelegramBot = require('node-telegram-bot-api');
 
-var TOKEN = process.env.BOT_TOKEN;
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.BOT_TOKEN;
 
-var bot = new TelegramBot( TOKEN, { polling: true } );
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
 
-bot.on('message', function(msg){
-  console.log('msg', msg);
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
 });
 
-var logErrorEcho = function logErrorEcho(msg) {
-  return function (err) {
-    return console.log(msg, err);
-  };
-};
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
 
-var logSuccessEcho = function(msg, match){
-  return function(data){
-    console.log( 'Success:', data);
-  };
-};
-
-var sendEcho = function(msg, match){
-  bot.sendMessage( msg.chat.id, match[ 1 ] )
-      .then( logSuccessEcho( msg, match ) )
-      .catch( logErrorEcho( 'Error:') );
-};
-
-bot.onText( /\/echo (.*)/, sendEcho);
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
