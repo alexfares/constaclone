e62655fb2ff033df289345d22903c7453f68855a
Alex Fares <alexanderfares@gmail.com>
2019-11-26 17:43:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.BOT_TOKEN || '';
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+//const bot = new TelegramBot(token, {polling: true});
+
+const port = process.env.PORT || 8443;
+const host = process.env.HOST;
+const bot = new TelegramBot(token, {webHook: {port: port, host: host}});
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
